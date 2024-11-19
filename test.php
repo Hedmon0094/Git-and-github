6f591e9fc2d83f086465259cb7b1658a062d31ae
Achacha Hedmon <hedmonachacha@gmail.com>
2024-11-18 21:46:58 -0800

    created a php file

---
+++ test.php
@@ -0,0 +1,28 @@
+
+    <?php
+$name="Hedmon O. Achacha";
+echo  "$name   Is learning PHP<br>";
+
+echo phpversion();
+
+$x=21;
+
+$y=99; 
+
+$z=21+99;
+echo "x +y is $z";
+var_dump($x);
+var_dump($name);
+$test=47.566;
+var_dump($test);
+
+$l=77;
+function globa(){
+    echo "l is a variable inside a function:$l";
+
+}
+globa();
+echo "l is a variable outside a fuction:$l";
+
+?>
+ 
